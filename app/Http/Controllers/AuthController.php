fa3d8868bc4877042eaf62466086fb7eead4f406
ab1dal <abidalauddin7@gmail.com>
2022-11-01 09:51:24 +0700

    fourth commit 'auth key'

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+        'user' => $user,
+        'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+       
+        //check password
+        if (!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+             'massage' => 'unauthorized'
+            ], 401);
+        }
+
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+        'user' => $user,
+        'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'massage' => 'Logged out'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeloController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,21 +19,22 @@ use App\Http\Controllers\BookController;
 */
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-   // return $request->user();
+ //   return $request->user();
 //});
 
-Route::get('halo', function(){
-    return ["me" => "Guanheng"];
+
+//public route
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+   
+    
 });
-
-Route::resource('siswa', SiswaController::class);
-
-// Route::get('books', [BookController::class, 'index']);
-// Route::get('books/{id}', [BookController::class, 'show']);
-// Route::get('books', [BookController::class, 'store']);
-// Route::get('books/{id}', [BookController::class, 'update']);
-// Route::get('books/{id}', [AuthController::class, 'destroy']);
-
-Route::resource('books', BookController::class)->except(
-    ['create','edit']
-);
